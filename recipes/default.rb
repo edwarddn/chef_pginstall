7daa21ec2b171bfc14d31d16c186181f0776de7a
Edward <edwarddn@gmail.com>
2016-05-22 21:19:22 -0300

    esquema

--- recipes/default.rb
+++ recipes/default.rb
@@ -32,68 +32,68 @@ cookbook_file "/etc/init.d/postgresql" do
   group "root"
 end
 
-#execute "link_pg" do 
-#  command "ln -s /usr/local/pgsql9.5.3/ /usr/local/pgsql"
-#  ignore_failure true
-#end
+execute "link_pg" do 
+  command "ln -s /usr/local/pgsql9.5.3/ /usr/local/pgsql"
+  ignore_failure true
+end
 
-#execute "start_default_pg" do 
-#  command "update-rc.d postgresql defaults"
-#end
+execute "start_default_pg" do 
+  command "update-rc.d postgresql defaults"
+end
 
-#directory "/database" do
-#  owner "postgres"
-#  group "postgres"
-#  mode 0755
-#  action :create
-#  ignore_failure true
-#end
-
-
-#execute "add_user_pg" do
-# command "useradd postgres --home-dir /database --shell /bin/bash"
-# ignore_failure true
-#end
-
-#cookbook_file "/var/lib/locales/supported.d/local" do 
-#  source "locale-gen"
-#   mode 0544
-#  owner "root"
-#  group "root"
-#end
-
-#execute "locale_gen" do
-# command "locale-gen"
-#end
-
-#cookbook_file "/etc/default/locale" do
-#  source "locale_etc"
-#  mode 0554
-#  owner "root"
-#  group "root"
-#end 
-
-#cookbook_file "/database/.profile" do
-#  source "profile_pg"
-#  owner "postgres"
-#  group "postgres"
-#  mode 0755
-#end
-
-#directory "/database/data" do
-#  owner "postgres"
-#  group "postgres"
-#  mode 0700
-#  action :create
-#end
-
-#execute "initdb" do
-#  command "su postgres -l -c 'initdb'"
-#  action :run
-#end
+directory "/database" do
+  owner "postgres"
+  group "postgres"
+  mode 0755
+  action :create
+  ignore_failure true
+end
 
 
-#service "postgresql" do
-# action :start
-#end
+execute "add_user_pg" do
+ command "useradd postgres --home-dir /database --shell /bin/bash"
+ ignore_failure true
+end
+
+cookbook_file "/var/lib/locales/supported.d/local" do 
+  source "locale-gen"
+   mode 0544
+  owner "root"
+  group "root"
+end
+
+execute "locale_gen" do
+ command "locale-gen"
+end
+
+cookbook_file "/etc/default/locale" do
+  source "locale_etc"
+  mode 0554
+  owner "root"
+  group "root"
+end 
+
+cookbook_file "/database/.profile" do
+  source "profile_pg"
+  owner "postgres"
+  group "postgres"
+  mode 0755
+end
+
+directory "/database/data" do
+  owner "postgres"
+  group "postgres"
+  mode 0700
+  action :create
+end
+
+execute "initdb" do
+  command "su postgres -l -c 'initdb'"
+  action :run
+end
+
+
+service "postgresql" do
+ action :start
+end
 
